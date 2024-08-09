16715d739ad452e59357d839dd5ac079683cdfe1
MartinBINARD <martin.binard.dev@gmail.com>
2024-08-08 16:46:08 -1000

    Add new jest.config.js file

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,11 @@
 module.exports = {
-  preset: 'react-native',
+    preset: 'react-native',
+    testEnvironment: 'node',
+    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+    transformIgnorePatterns: [
+        'node_modules/(?!((jest-)?react-native|@react-native(-community)?)|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|react-native-svg|i18n-js)',
+        'jest-runner',
+    ],
+    testPathIgnorePatterns: ['<rootDir>/node_modules/', '@react-native'],
+    testMatch: ['**/*.test.ts?(x)', '**/*.test.js?(x)'],
 };
