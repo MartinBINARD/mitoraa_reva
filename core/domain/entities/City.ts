12708b16392a430a18d794985d0b6fdfb503fc40
MartinBINARD <martin.binard.dev@gmail.com>
2024-08-16 17:53:17 -1000

    Remove population and country in City entity

--- core/domain/entities/City.ts
+++ core/domain/entities/City.ts
@@ -3,8 +3,6 @@ import { Location } from './Location';
 export interface City {
     name: string;
     coord: Location;
-    country: string;
-    population: number;
     sunrise: number;
     sunset: number;
 }
