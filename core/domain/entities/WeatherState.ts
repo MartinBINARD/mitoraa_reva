1dfc14cb7661bf28479f4dcf12c85caab5432c6e
MartinBINARD <martin.binard.dev@gmail.com>
2024-08-21 14:40:28 -1000

    Add HourlyWeather UniteDegree WeatherState entities

---
+++ core/domain/entities/WeatherState.ts
@@ -0,0 +1,11 @@
+export enum WeatherState {
+    clear_sky = 'clear sky',
+    few_clouds = 'few clouds',
+    scattered_clouds = 'scattered clouds',
+    broken_clouds = 'broken clouds',
+    shower_rain = 'shower rain',
+    rain = 'rain',
+    thunderstorm = 'thunderstorm',
+    snow = 'snow',
+    mist = 'mist',
+}
