429abdff3c56e84feb7a0c3f409f922ac39b0704
MartinBINARD <martin.binard.dev@gmail.com>
2024-08-23 15:55:50 -1000

    Add Fahrenheit display test to GetCityDailyWeatherUseCase

--- core/domain/test/usecases/GetCityDailyWeatherUseCase.test.ts
+++ core/domain/test/usecases/GetCityDailyWeatherUseCase.test.ts
@@ -1,37 +1,62 @@
 import { DailyWeather } from '@core/domain/src/entities/DailyWeather';
 import { WeatherState } from '@core/domain/src/entities/WeatherState';
 import { GetDailyWeatherRequest } from '@core/domain/src/ports/requests/GetWeatherRequest';
+import { GetCityDailyWeatherUseCase } from '@core/domain/src/usecases/GetCityDailyWeatherUseCase';
 import { GetDailyWeatherPresenterBuilder } from '../builders/GetDailyWeatherPresenterBuilder';
 import { WeatherRepositoryBuilder } from '../builders/WeatherRepositoryBuilder';
-import { GetCityDailyWeatherUseCase } from '@core/domain/src/usecases/GetCityDailyWeatherUseCase';
 
 describe('Get city daily weather use case', () => {
-    it('Should display daily weather in °C for next days', async () => {
-        const weatherData: DailyWeather[] = [
-            {
-                type: 'daily',
-                day: '22/08/2021',
-                temperatureMin: 22,
-                temperatureMax: 27,
-                weather: WeatherState.clear_sky,
-                unite: 'C',
-                sunrise: '6:12',
-                sunset: '17:51',
-            },
-            {
-                type: 'daily',
-                day: '23/08/2021',
-                temperatureMin: 23,
-                temperatureMax: 28,
-                weather: WeatherState.few_clouds,
-                unite: 'C',
-                sunrise: '6:12',
-                sunset: '17:51',
-            },
-        ];
+    const weatherDataInCelsius: DailyWeather[] = [
+        {
+            type: 'daily',
+            day: '22/08/2021',
+            temperatureMin: 22,
+            temperatureMax: 27,
+            weather: WeatherState.clear_sky,
+            unite: 'C',
+            sunrise: '6:12',
+            sunset: '17:51',
+        },
+        {
+            type: 'daily',
+            day: '23/08/2021',
+            temperatureMin: 23,
+            temperatureMax: 28,
+            weather: WeatherState.few_clouds,
+            unite: 'C',
+            sunrise: '6:12',
+            sunset: '17:51',
+        },
+    ];
 
+    const weatherDataInFahrenheit: DailyWeather[] = [
+        {
+            type: 'daily',
+            day: '22/08/2021',
+            temperatureMin: 71.6,
+            temperatureMax: 80.6,
+            weather: WeatherState.clear_sky,
+            unite: 'F',
+            sunrise: '6:12',
+            sunset: '17:51',
+        },
+        {
+            type: 'daily',
+            day: '23/08/2021',
+            temperatureMin: 73.4,
+            temperatureMax: 82.4,
+            weather: WeatherState.few_clouds,
+            unite: 'F',
+            sunrise: '6:12',
+            sunset: '17:51',
+        },
+    ];
+
+    it('Should display daily weather in C° for next days', async () => {
         return new Promise<DailyWeather[]>((resolve) => {
-            const weatherRepository = new WeatherRepositoryBuilder().withGetCityWeekWeathe((_) => Promise.resolve(weatherData)).build();
+            const weatherRepository = new WeatherRepositoryBuilder()
+                .withGetCityWeekWeather((_) => Promise.resolve(weatherDataInCelsius))
+                .build();
             const useCase = new GetCityDailyWeatherUseCase(weatherRepository);
             const weatherRequest = new GetDailyWeatherRequest('Papeete', 'C');
 
@@ -43,7 +68,27 @@ describe('Get city daily weather use case', () => {
         }).then((weather: DailyWeather[]) => {
             expect(weather).not.toBeNull();
             expect(weather).toHaveLength(2);
-            expect(weather).toEqual(weatherData);
+            expect(weather).toEqual(weatherDataInCelsius);
+        });
+    });
+
+    it('Should display daily weather in F° for next days', async () => {
+        return new Promise<DailyWeather[]>((resolve) => {
+            const weatherRepository = new WeatherRepositoryBuilder()
+                .withGetCityWeekWeather((_) => Promise.resolve(weatherDataInCelsius))
+                .build();
+            const useCase = new GetCityDailyWeatherUseCase(weatherRepository);
+            const weatherRequest = new GetDailyWeatherRequest('Papeete', 'F');
+
+            const presenter = new GetDailyWeatherPresenterBuilder()
+                .withDisplayWeather((weather: DailyWeather[]) => resolve(weather))
+                .build();
+
+            useCase.execute(weatherRequest, presenter);
+        }).then((weather: DailyWeather[]) => {
+            expect(weather).not.toBeNull();
+            expect(weather).toHaveLength(2);
+            expect(weather).toEqual(weatherDataInFahrenheit);
         });
     });
 });
