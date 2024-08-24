5939ebbc0a4d90534a17bce63d617fc774b1bba4
MartinBINARD <martin.binard.dev@gmail.com>
2024-08-23 16:00:13 -1000

    Add conversion to Fahrenheit to GetCityDailyUseCase

--- core/domain/src/usecases/GetCityDailyWeatherUseCase.ts
+++ core/domain/src/usecases/GetCityDailyWeatherUseCase.ts
@@ -1,3 +1,4 @@
+import { DailyWeather } from '../entities/DailyWeather';
 import { GetDailyWeatherPresenter } from '../ports/presenters/GetDailyWeatherPresenter';
 import { WeatherRepository } from '../ports/repositories/WeatherRepository';
 import { GetDailyWeatherRequest } from '../ports/requests/GetWeatherRequest';
@@ -5,8 +6,27 @@ import { GetDailyWeatherRequest } from '../ports/requests/GetWeatherRequest';
 export class GetCityDailyWeatherUseCase {
     constructor(private readonly weatherRepository: WeatherRepository) {}
 
+    private convertToFarenheit(temperature: number) {
+        return temperature * (9 / 5) + 32;
+    }
+
     async execute(request: GetDailyWeatherRequest, presenter: GetDailyWeatherPresenter) {
         presenter.displayLoadingWeather();
-        presenter.displayDailyWeather(await this.weatherRepository.getCityDailyWeather(request.city));
+        const weekWeather: DailyWeather[] = await this.weatherRepository
+            .getCityDailyWeather(request.city)
+            .then((weekWeather: DailyWeather[]) => {
+                if (request.unite == 'F') {
+                    return weekWeather.map((dayWeather) => {
+                        dayWeather.unite = 'F';
+                        dayWeather.temperatureMax = this.convertToFarenheit(dayWeather.temperatureMax);
+                        dayWeather.temperatureMin = this.convertToFarenheit(dayWeather.temperatureMin);
+
+                        return dayWeather;
+                    });
+                } else {
+                    return weekWeather;
+                }
+            });
+        presenter.displayDailyWeather(weekWeather);
     }
 }
