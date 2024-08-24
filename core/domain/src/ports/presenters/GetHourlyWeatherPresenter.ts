67a6e94afab340e5a4c379d5e34a9c2d8087eafa
MartinBINARD <martin.binard.dev@gmail.com>
2024-08-23 18:54:53 -1000

    Fix typo to GetHourlyWeatherPresenter

--- core/domain/src/ports/presenters/GetHourlyWeatherPresenter.ts
+++ core/domain/src/ports/presenters/GetHourlyWeatherPresenter.ts
@@ -2,5 +2,5 @@ import { HourlyWeather } from '../../entities/HourlyWeather';
 
 export interface GetHourlyWeatherPresenter {
     displayLoadingWeather(): void;
-    displayDailyWeather(weather: HourlyWeather[]): void;
+    displayHourlyWeather(weather: HourlyWeather[]): void;
 }
