76a4a8a08da1ec5f1de870e5a7d69d8bb0c4e290
MartinBINARD <martin.binard.dev@gmail.com>
2024-08-23 19:06:03 -1000

    Add metric display test to GetCityHourlyWeatherUseCase

---
+++ core/domain/test/usecases/GetCityHourlyWeatherUseCase.test.ts
@@ -0,0 +1,56 @@
+import { HourlyWeather } from '@core/domain/src/entities/HourlyWeather';
+import { WeatherState } from '@core/domain/src/entities/WeatherState';
+import { GetHourlyWeatherRequest } from '@core/domain/src/ports/requests/GetHourlyWeatherRequest';
+import { GetHourlyWeatherPresenterBuilder } from '../builders/GetHourlyWeatherPresenterBuilder';
+import { WeatherRepositoryBuilder } from '../builders/WeatherRepositoryBuilder';
+
+describe('Get city hourly weather use case', () => {
+    const weatherDataMetric: HourlyWeather[] = [
+        {
+            type: 'hourly',
+            time: '7:00',
+            temperature: 25,
+            weather: WeatherState.clear_sky,
+            unit: 'C',
+            humidity: 90,
+            wind: { speed: 5, direction: 50, unit: 'km/s' },
+        },
+        {
+            type: 'hourly',
+            time: '8:00',
+            temperature: 27,
+            weather: WeatherState.clear_sky,
+            unit: 'C',
+            humidity: 90,
+            wind: { speed: 7.2, direction: 50, unit: 'km/s' },
+        },
+        {
+            type: 'hourly',
+            time: '9:00',
+            temperature: 29,
+            weather: WeatherState.clear_sky,
+            unit: 'C',
+            humidity: 90,
+            wind: { speed: 7.5, direction: 55, unit: 'km/s' },
+        },
+    ];
+
+    it('Should display daily weather in C° for next hours', async () => {
+        return new Promise<HourlyWeather>((resolve) => {
+            const weatherRepository = new WeatherRepositoryBuilder()
+                .withGetCityHourlyWeather((_) => Promise.resolve(weatherDataMetric))
+                .build();
+            const useCase = new GetCityHourlyWeatherUseCase(weatherRepository);
+            const weatherRequest = new GetHourlyWeatherRequest('Papeete', 'C', 'km/s');
+            const presenter = new GetHourlyWeatherPresenterBuilder()
+                .withDisplayWeather((weather: HourlyWeather[]) => resolve(weather))
+                .build();
+
+            useCase.execute(weatherRequest, presenter);
+        }).then((weather: HourlyWeather[]) => {
+            expect(weather).not.toBeNull();
+            expect(weather).toHaveLength(3);
+            expect(weather).toBe(weatherDataMetric);
+        });
+    });
+});
