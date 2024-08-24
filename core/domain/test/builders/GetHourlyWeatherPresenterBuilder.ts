9aa87cda9ef8be3b08d3d68fdc1b9a44492f7329
MartinBINARD <martin.binard.dev@gmail.com>
2024-08-23 17:30:54 -1000

    Add GetHourlyWeatherPresenterBuilder

---
+++ core/domain/test/builders/GetHourlyWeatherPresenterBuilder.ts
@@ -0,0 +1,19 @@
+import { HourlyWeather } from '@core/domain/src/entities/HourlyWeather';
+import { GetHourlyWeatherPresenter } from '@core/domain/src/ports/presenters/GetHourlyWeatherPresenter';
+
+export class GetHourlyWeatherPresenterBuilder {
+    private displayLoadingWeather: () => void = () => null;
+    private displayWeather: (weather: HourlyWeather[]) => void = () => null;
+
+    withDisplayWeather(displayWeather: (weather: HourlyWeather[]) => void) {
+        this.displayWeather = displayWeather;
+        return this;
+    }
+
+    build(): GetHourlyWeatherPresenter {
+        return {
+            displayLoadingWeather: this.displayLoadingWeather,
+            displayDailyWeather: this.displayWeather,
+        };
+    }
+}
