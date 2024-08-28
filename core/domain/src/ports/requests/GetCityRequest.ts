98eb31f72e1c88b9d7bd5155060a2e287893c2f6
MartinBINARD <martin.binard.dev@gmail.com>
2024-08-28 12:12:13 -1000

    Remove GetCityRequest and GetCityPresenter

--- core/domain/src/ports/requests/GetCityRequest.ts
+++
@@ -1,3 +0,0 @@
-export class GetCityRequest {
-    constructor(public city: string) {}
-}
--- core/domain/test/builders/CityRepositoryBuilder.ts
+++ core/domain/test/builders/CityRepositoryBuilder.ts
@@ -2,14 +2,7 @@ import { City } from '@core/domain/src/entities/City';
 import { CityRepository } from '@core/domain/src/ports/repositories/CityRepository';
 
 export class CityRepositoryBuilder {
-    private getCity: (city: string) => Promise<City> = () =>
-        Promise.resolve({ name: 'Papeete', location: { latitude: -17.689001, longitude: -149.5849327 } });
     private getCities: () => Promise<City[]> = () => Promise.resolve([]);
-
-    withGetCity(getCity: (city: string) => Promise<City>) {
-        this.getCity = getCity;
-        return this;
-    }
 
     withGetCities(getCities: () => Promise<City[]>) {
         this.getCities = getCities;
@@ -18,7 +11,6 @@ export class CityRepositoryBuilder {
 
     build(): CityRepository {
         return {
-            getCity: this.getCity,
             getCities: this.getCities,
         };
     }
