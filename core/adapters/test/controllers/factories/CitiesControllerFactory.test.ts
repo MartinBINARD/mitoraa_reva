c3e70a995c901ffb66bad701625e85526a12e729
MartinBINARD <martin.binard.dev@gmail.com>
2024-08-30 12:55:35 -1000

    Add test to CitiesControllerFactory

---
+++ core/adapters/test/controllers/factories/CitiesControllerFactory.test.ts
@@ -0,0 +1,11 @@
+import { GetCitiesUseCaseBuilder } from '../../builders/GetCitiesUseCaseBuilder';
+
+describe('Cities Controller Factory', () => {
+    it('Create Cities  Presenter', () => {
+        const factory = new CitiesControllerFactory(new GetCitiesUseCaseBuilder().build());
+
+        const presenter = factory.build();
+
+        expect(presenter).not.toBeNull();
+    });
+});
