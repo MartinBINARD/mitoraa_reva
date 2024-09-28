563187744ac1505e4c17d70ddcbac81f86409514
MartinBINARD <martin.binard.dev@gmail.com>
2024-09-27 21:09:50 -1000

    Fix : loose option to @babel/plugin-transform-private-methods incompatibility with assumptions property

--- babel.config.js
+++ babel.config.js
@@ -13,8 +13,4 @@ module.exports = {
         ],
         ['@babel/plugin-transform-private-methods', { loose: true }],
     ],
-    assumptions: {
-        privateFieldsAsProperties: true,
-        setPublicClassFields: true,
-    },
 };
