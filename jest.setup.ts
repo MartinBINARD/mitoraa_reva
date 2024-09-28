a07c9abf3ffd2b372aa66db6d753f35b644b8208
MartinBINARD <martin.binard.dev@gmail.com>
2024-09-27 20:45:45 -1000

    Fix public class issue in packages

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,6 +1,6 @@
-import mockSafeAreaContext from 'react-native-safe-area-context/jest/mock';
-import 'react-native-gesture-handler/jestSetup';
 import '@testing-library/jest-native/extend-expect';
+import 'react-native-gesture-handler/jestSetup';
+import mockSafeAreaContext from 'react-native-safe-area-context/jest/mock';
 
 jest.mock('react-native-safe-area-context', () => mockSafeAreaContext);
 
