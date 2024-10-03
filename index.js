c5a4d17c329ea2d2717fcc0b0adc10680330d2f6
MartinBINARD <martin.binard.dev@gmail.com>
2024-10-02 18:33:53 -1000

    Add new app icon and bootsplash icon

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 /**
  * @format
  */
-
+import React from 'react';
 import { AppRegistry } from 'react-native';
+import RNBootSplash from 'react-native-bootsplash';
 import { name as appName } from './app.json';
 import App from './src/App';
 
-AppRegistry.registerComponent(appName, () => App);
+function Root() {
+    return <App hideSplashScreen={() => RNBootSplash.hide({ fade: true })} />;
+}
+
+AppRegistry.registerComponent(appName, () => Root);
+export default Root;
